01b0ef87b6ce785ce76da1734a2d61d06d330da2
veerannadeshanur11 <shreyasdeshanur34@gmail.com>
2025-10-08 00:00:12 +0530

    added 10% bonus

--- salarybonus.py
+++ salarybonus.py
@@ -3,3 +3,12 @@
 salary = float(input("Enter the employee's salary: "))
 
 print("\nOriginal Salary:", int(salary))
+
+# Salary Bonus Calculator Program (with 10% bonus)
+
+salary = float(input("Enter the employee's salary: "))
+
+bonus_salary = salary + (salary * 0.10)
+
+print("\nOriginal Salary:", int(salary))
+print("Bonus Added:", int(bonus_salary))
